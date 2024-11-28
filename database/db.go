f6e86455b6df383bd1fcf366fa582f28bb0be53f
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-11-28 02:04:39 +0100

    Create db.go

---
+++ database/db.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=2323 dbname=webapp port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
